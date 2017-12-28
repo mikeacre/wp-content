1233cd86bf4b541ebb5a6e58bd0cbdfa02a7ac9d
mikeacre <mikeacre@gmail.com>
2017-12-28 12:48:28 -0500

    try again
    yup really

--- themes/storefront/functions.php
+++ themes/storefront/functions.php
@@ -19,7 +19,7 @@ function remove_storefront_sidebar() {
 	}
 }
 
-add_filter( 'add_to_cart_text', 'woo_custom_cart_button_text' );    // < 2.1
+add_filter( 'woocommerce_product_single_add_to_cart_text', 'woo_custom_cart_button_text' );    // 2.1 +
 
 function woo_custom_cart_button_text() {
 
