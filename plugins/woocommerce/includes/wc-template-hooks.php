4e675564be0b46dee96d8b966dcecc8ffc1560a9
mikeacre <mikeacre@gmail.com>
2017-12-29 12:00:26 -0500

    loop hook
    final?

--- plugins/woocommerce/includes/wc-template-hooks.php
+++ plugins/woocommerce/includes/wc-template-hooks.php
@@ -91,17 +91,21 @@ add_action( 'woocommerce_no_products_found', 'wc_no_products_found' );
  * @see woocommerce_template_loop_rating()
  */
 add_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
+
+add_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 20 );
+add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 5 );
+add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_add_to_cart', 10 );
+
 add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 5 );
-add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_add_to_cart', 30 );
-add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 20 );
-add_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
+
+add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_price', 10 );
+add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_rating', 5 );
 
 add_action( 'woocommerce_before_subcategory', 'woocommerce_template_loop_category_link_open', 10 );
 add_action( 'woocommerce_shop_loop_subcategory_title', 'woocommerce_template_loop_category_title', 10 );
 add_action( 'woocommerce_after_subcategory', 'woocommerce_template_loop_category_link_close', 10 );
 
-add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
-add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_rating', 5 );
+
 
 /**
  * Subcategories.
