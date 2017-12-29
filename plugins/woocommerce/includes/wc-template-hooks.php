4190dbc641fc4a7cfa84738da2613f6faf258352
mikeacre <mikeacre@gmail.com>
2017-12-29 15:39:23 -0500

    d
    d

--- plugins/woocommerce/includes/wc-template-hooks.php
+++ plugins/woocommerce/includes/wc-template-hooks.php
@@ -97,8 +97,8 @@ add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop
 add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_add_to_cart', 20 );
 
 add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_price', 10 );
-add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_rating', 15 );
-add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 20 );
+add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_rating', 20 );
+add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_product_link_close', 30 );
 
 add_action( 'woocommerce_before_subcategory', 'woocommerce_template_loop_category_link_open', 10 );
 add_action( 'woocommerce_shop_loop_subcategory_title', 'woocommerce_template_loop_category_title', 10 );
