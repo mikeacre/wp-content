756143bc728e611ed47db2a5087f88b5701a1623
mikeacre <mikeacre@gmail.com>
2018-01-26 22:18:45 -0500

    new_layout-1

--- themes/storefront/index.php
+++ themes/storefront/index.php
@@ -1,34 +1,259 @@
-<?php
-/**
- * The main template file.
- *
- * This is the most generic template file in a WordPress theme
- * and one of the two required files for a theme (the other being style.css).
- * It is used to display a page when nothing more specific matches a query.
- * E.g., it puts together the home page when no home.php file exists.
- * Learn more: http://codex.wordpress.org/Template_Hierarchy
- *
- * @package storefront
- */
+<!DOCTYPE html>
+<html >
+<head>
 
-get_header(); ?>
+  <meta charset="UTF-8">
+  <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
-	<div id="primary" class="content-area">
-		<main id="main" class="site-main" role="main">
+  <meta name="viewport" content="width=device-width, initial-scale=1, minimum-scale=1">
+  <link rel="shortcut icon" href="assets/images/nocoiner-1-1000x500.png" type="image/x-icon">
+  <meta name="description" content="">
+  <title>Blog</title>
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-grid.min.css">
+  <link rel="stylesheet" href="assets/bootstrap/css/bootstrap-reboot.min.css">
+  <link rel="stylesheet" href="assets/shopping-cart/minicart-theme.css">
+  <link rel="stylesheet" href="assets/socicon/css/styles.css">
+  <link rel="stylesheet" href="assets/animatecss/animate.min.css">
+  <link rel="stylesheet" href="assets/dropdown/css/style.css">
+  <link rel="stylesheet" href="assets/theme/css/style.css">
+  <link rel="stylesheet" href="assets/custom/css/mbr-additional.css" type="text/css">
 
-		<?php if ( have_posts() ) :
 
-			get_template_part( 'loop' );
 
-		else :
+</head>
+<body>
+<script>var shopcartSettings = {"shopcart_top_offset":85,"side_offset":550,"site_width":1150,"shopcart_icon_color":"#FFFFFF","shopcart_back_color":"#d14841","shopcart_icon_size":30,"sc_count_color":"#FFFFFF","sc_count_back_color":"#7ac673","sc_count_size":12,"checkout_button":"Check out with"};</script>
+<section class="menu popup-btn-cards cid-qH1Y4lft0H" once="menu" id="menu4-e">
 
-			get_template_part( 'content', 'none' );
 
-		endif; ?>
 
-		</main><!-- #main -->
-	</div><!-- #primary -->
 
-<?php
-do_action( 'storefront_sidebar' );
-get_footer();
+  <nav class="navbar navbar-dropdown navbar-fixed-top navbar-expand-lg">
+      <div class="mbr-overlay" style="opacity: 1;"></div>
+
+
+    <div class="collapse navbar-collapse" id="navbarSupportedContent">
+
+      <div class="left-menu">
+        <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item">
+                <a class="nav-link link text-black display-4" href="index.html">
+                    Shirts</a>
+            </li>
+            <li class="nav-item">
+                <a class="nav-link link text-black display-4" href="blog.html">
+                     Blog</a>
+            </li></ul>
+      </div>
+
+      <div class="brand-container">
+        <div class="navbar-brand">
+            <span class="navbar-logo">
+                <a href="index.html">
+                    <img src="assets/images/nocoiner-1-1000x500.png" alt="" title="" style="height: 3.9rem;">
+                </a>
+            </span>
+
+        </div>
+      </div>
+
+      <div class="right-menu">
+        <ul class="navbar-nav nav-dropdown" data-app-modern-menu="true"><li class="nav-item">
+              <a class="nav-link link text-black display-4" href="about.html">
+                  About Us</a>
+          </li><li class="nav-item"><a class="nav-link link text-black display-4" href="contact.html">
+                  Contact</a></li></ul>
+      </div>
+    </div>
+
+        <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+          <div class="hamburger">
+              <span></span>
+              <span></span>
+              <span></span>
+              <span></span>
+          </div>
+        </button>
+  </nav>
+</section>
+
+<section class="cid-qH2lljIiVR" id="content7-g">
+
+
+
+
+    <div class="container-fluid">
+        <div class="row justify-content-center">
+            <div class="col-12 col-lg-6 col-md-8 align-center">
+
+                <h2 class="mbr-section-title align-center mbr-fonts-style mbr-bold display-2">
+                    BLOG</h2>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section article svg-gradient-elements cid-qH2lm0bGkH" id="content6-h">
+
+
+
+    <svg class="svg-gradient">
+        <linearGradient>
+            <stop class="gradColor1" offset="0%" stop-color="#a38dfd"></stop>
+            <stop class="gradColor2" offset="100%" stop-color="#7ccbf1"></stop>
+        </linearGradient>
+    </svg>
+    <div class="container-fluid">
+        <div class="media-container-row">
+            <div class="col-12 col-md-8 col-lg-8">
+                <h2 class="mbr-section-title pb-3 align-left mbr-fonts-style mbr-bold display-2">
+                    BLOCKQUOTE
+                </h2>
+                <div class="blockquote-quote align-left pb-3">
+                    <svg class="gradient-element" xmlns="http://www.w3.org/2000/svg" xmlns:xlink="http://www.w3.org/1999/xlink" version="1.1" x="0px" y="0px" width="475.082px" height="475.081px" viewBox="0 0 475.082 475.081" xml:space="preserve">
+                        <g><g>
+                            <path d="M164.45,219.27h-63.954c-7.614,0-14.087-2.664-19.417-7.994c-5.327-5.33-7.994-11.801-7.994-19.417v-9.132    c0-20.177,7.139-37.401,21.416-51.678c14.276-14.272,31.503-21.411,51.678-21.411h18.271c4.948,0,9.229-1.809,12.847-5.424    c3.616-3.617,5.424-7.898,5.424-12.847V54.819c0-4.948-1.809-9.233-5.424-12.85c-3.617-3.612-7.898-5.424-12.847-5.424h-18.271    c-19.797,0-38.684,3.858-56.673,11.563c-17.987,7.71-33.545,18.132-46.68,31.267c-13.134,13.129-23.553,28.688-31.262,46.677    C3.855,144.039,0,162.931,0,182.726v200.991c0,15.235,5.327,28.171,15.986,38.834c10.66,10.657,23.606,15.985,38.832,15.985    h109.639c15.225,0,28.167-5.328,38.828-15.985c10.657-10.663,15.987-23.599,15.987-38.834V274.088    c0-15.232-5.33-28.168-15.994-38.832C192.622,224.6,179.675,219.27,164.45,219.27z"></path>
+                            <path d="M459.103,235.256c-10.656-10.656-23.599-15.986-38.828-15.986h-63.953c-7.61,0-14.089-2.664-19.41-7.994    c-5.332-5.33-7.994-11.801-7.994-19.417v-9.132c0-20.177,7.139-37.401,21.409-51.678c14.271-14.272,31.497-21.411,51.682-21.411    h18.267c4.949,0,9.233-1.809,12.848-5.424c3.613-3.617,5.428-7.898,5.428-12.847V54.819c0-4.948-1.814-9.233-5.428-12.85    c-3.614-3.612-7.898-5.424-12.848-5.424h-18.267c-19.808,0-38.691,3.858-56.685,11.563c-17.984,7.71-33.537,18.132-46.672,31.267    c-13.135,13.129-23.559,28.688-31.265,46.677c-7.707,17.987-11.567,36.879-11.567,56.674v200.991    c0,15.235,5.332,28.171,15.988,38.834c10.657,10.657,23.6,15.985,38.828,15.985h109.633c15.229,0,28.171-5.328,38.827-15.985    c10.664-10.663,15.985-23.599,15.985-38.834V274.088C475.082,258.855,469.76,245.92,459.103,235.256z"></path>
+                        </g></g>
+                    </svg>
+                </div>
+                <div class="mbr-text mbr-fonts-style pb-3 display-5">
+                    Lorem ipsum dolor sit amet, consectetur adipisicing elit. Pariatur facilis expedita cum iste voluptate itaque doloribus molestias numquam natus voluptates, molestiae tempora praesentium ratione dicta laudantium, excepturi voluptatibus, assumenda repellendus!
+                </div>
+                <div class="mbr-text signature mbr-fonts-style align-left display-7">
+                    by <span> John Smith</span>
+                </div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section article content1 cid-qH2p3FdrZA" id="content2-r">
+
+
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="mbr-text col-12 col-md-8 mbr-fonts-style display-7">
+                <blockquote><span style="font-style: normal;">Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Venenatis lectus magna fringilla urna porttitor rhoncus dolor. Mattis pellentesque id nibh tortor id aliquet lectus proin. Nulla pellentesque dignissim enim sit amet venenatis urna cursus. Diam ut venenatis tellus in metus vulputate eu scelerisque. Est sit amet facilisis magna etiam tempor. Morbi enim nunc faucibus a pellentesque sit. Massa placerat duis ultricies lacus sed turpis. Mi bibendum neque egestas congue quisque. Fringilla ut morbi tincidunt augue interdum velit euismod in.</span></blockquote><blockquote><span style="font-style: normal;">Dignissim cras tincidunt lobortis feugiat vivamus at. Mollis nunc sed id semper risus in hendrerit gravida rutrum. Elit duis tristique sollicitudin nibh sit. Pharetra sit amet aliquam id. Sit amet volutpat consequat mauris nunc. Mattis aliquam faucibus purus in massa tempor nec feugiat. Facilisi cras fermentum odio eu. Nibh sit amet commodo nulla facilisi nullam vehicula. Dui accumsan sit amet nulla facilisi morbi tempus iaculis urna. Quam id leo in vitae turpis massa sed elementum. Cursus vitae congue mauris rhoncus aenean vel. Tempus urna et pharetra pharetra massa massa ultricies mi quis. Pellentesque diam volutpat commodo sed egestas egestas. Condimentum vitae sapien pellentesque habitant morbi tristique senectus. Donec et odio pellentesque diam volutpat commodo. Sed risus ultricies tristique nulla aliquet enim tortor at. Augue ut lectus arcu bibendum at varius vel. Euismod quis viverra nibh cras. Nulla aliquet porttitor lacus luctus accumsan tortor posuere ac ut.</span></blockquote><blockquote><span style="font-style: normal;">Orci sagittis eu volutpat odio. Lorem donec massa sapien faucibus. Mauris sit amet massa vitae tortor condimentum lacinia quis vel. Metus dictum at tempor commodo ullamcorper. Sed sed risus pretium quam vulputate dignissim. Morbi tristique senectus et netus et malesuada fames. Consectetur lorem donec massa sapien faucibus et. Amet facilisis magna etiam tempor orci eu lobortis elementum nibh. Congue mauris rhoncus aenean vel elit scelerisque mauris pellentesque. Lacinia at quis risus sed vulputate odio. Sem nulla pharetra diam sit amet nisl suscipit adipiscing. A cras semper auctor neque vitae tempus quam pellentesque nec. Neque aliquam vestibulum morbi blandit cursus risus. Non pulvinar neque laoreet suspendisse interdum consectetur libero id. Metus dictum at tempor commodo ullamcorper. Tellus id interdum velit laoreet. Amet dictum sit amet justo donec enim diam vulputate ut. Vulputate sapien nec sagittis aliquam malesuada bibendum arcu. Sagittis nisl rhoncus mattis rhoncus urna neque.</span></blockquote>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="mbr-section article content1 cid-qH2lB1vRme" id="content1-j">
+
+
+
+    <div class="container">
+        <div class="media-container-row">
+            <div class="mbr-text col-12 col-md-8 col-lg-10 mbr-fonts-style display-7">
+                <div>&nbsp;&nbsp;&nbsp;&nbsp;<strong>Lorem ipsum dolor sit amet, consectetur adipisicing elit.</strong> <a class="text-primary" href="https://google.com/">Veritatis eveniet</a> minima dignissimos quo autem debitis, ducimus assumenda blanditiis asperiores tempora doloribus cum molestiae excepturi fuga velit voluptatem quibusdam exercitationem ipsa.</div>
+                <br>
+                <div>&nbsp;&nbsp;&nbsp;&nbsp;<em>Amet porro sint ratione?</em> Quaerat, praesentium, perspiciatis. <a class="text-secondary" href="https://google.com/"> Architecto perferendis </a> corrupti necessitatibus. Quisquam earum adipisci alias, vero autem possimus est ut modi, doloribus, dignissimos quod, voluptatem facilis nulla maiores aperiam odit.</div>
+            </div>
+        </div>
+    </div>
+</section>
+
+<section class="cid-qH2kr71eik" id="footer4-f">
+
+
+
+
+
+    <div class="container">
+        <div class="row justify-content-center">
+            <div class="first-column col-md-4 align-left">
+                <ul>
+                    <li class="first-column-item mbr-fonts-style display-4">
+                        <a href="#" class="text-white">Return policy</a>
+                    </li>
+                    <li class="first-column-item mbr-fonts-style display-4"></li>
+                    <li class="first-column-item mbr-fonts-style display-4">
+                        <a href="page1.html" class="text-white">Blog</a>
+                    </li>
+                    <li class="first-column-item mbr-fonts-style display-4">
+                        <a href="#" class="text-white">Contact us</a>
+                    </li>
+                    <li class="first-column-item mbr-fonts-style display-4">
+                        <a href="page2.html" class="text-white">About us</a>
+                    </li>
+                </ul>
+            </div>
+            <div class="second-column align-left col-md-4">
+                <ul>
+                    <li class="second-column-item mbr-fonts-style display-4">
+                        <span class="mbr-iconfont socicon-facebook socicon"></span>
+                        <a href="#" class="text-white">Facebook</a>
+                    </li>
+                    <li class="second-column-item mbr-fonts-style display-4">
+                        <span class="mbr-iconfont socicon-twitter socicon"></span>
+                        <a href="#" class="text-white">Twitter</a>
+                    </li>
+                    <li class="second-column-item mbr-fonts-style display-4">
+                        <span class="mbr-iconfont socicon-instagram socicon"></span>
+                        <a href="#" class="text-white">Instagram</a>
+                    </li>
+                    <li class="second-column-item mbr-fonts-style display-4">
+                        <span class="mbr-iconfont socicon-youtube socicon"></span>
+                        <a href="#" class="text-white">Youtube</a>
+                    </li>
+                    <li class="second-column-item mbr-fonts-style display-4"></li>
+
+                </ul>
+            </div>
+            <div class="align-right col-md-4">
+
+                <ul class="card-support">
+                    <li>
+                        <img src="assets/images/visa.png" height="30" alt="Visa">
+                    </li>
+                    <li>
+                        <img src="assets/images/master-card.png" height="30" alt="Master Card">
+                    </li>
+                    <li>
+                        <img src="assets/images/american-express.png" height="30" alt="American Express">
+                    </li>
+                    <li>
+                        <img src="assets/images/paypal.png" height="30" alt="PayPal">
+                    </li>
+
+                    <li>
+                        <img src="assets/images/discover.png" height="30" alt="Discover">
+                    </li>
+                    <li>
+                        <img src="assets/images/bitcoin.png" height="30" alt="Bitcoin">
+                    </li>
+
+
+                    <li>
+                        <img src="assets/images/stripe.png" height="30" alt="Stripe">
+                    </li>
+
+
+
+                </ul>
+            </div>
+        </div>
+    </div>
+</section>
+
+
+  <script src="assets/web/assets/jquery/jquery.min.js"></script>
+  <script src="assets/popper/popper.min.js"></script>
+  <script src="assets/bootstrap/js/bootstrap.min.js"></script>
+  <script src="assets/mbr-gradient-svg/mbr-gradient-svg.js"></script>
+  <script src="assets/smoothscroll/smooth-scroll.js"></script>
+  <script src="assets/shopping-cart/minicart.js"></script>
+  <script src="assets/shopping-cart/jquery.easing.min.js"></script>
+  <script src="assets/shopping-cart/minicart-customizer.js"></script>
+  <script src="assets/touchswipe/jquery.touch-swipe.min.js"></script>
+  <script src="assets/viewportchecker/jquery.viewportchecker.js"></script>
+  <script src="assets/dropdown/js/script.min.js"></script>
+  <script src="assets/theme/js/script.js"></script>
+
+
+  <input name="animation" type="hidden">
+  </body>
+</html>
