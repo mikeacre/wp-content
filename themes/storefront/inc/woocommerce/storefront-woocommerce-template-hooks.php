adfd52ca8278d78eecc08e9cc6e685cc21153246
mikeacre <mikeacre@gmail.com>
2018-01-29 08:50:09 -0500

    d
    d

--- themes/storefront/inc/woocommerce/storefront-woocommerce-template-hooks.php
+++ themes/storefront/inc/woocommerce/storefront-woocommerce-template-hooks.php
@@ -22,11 +22,16 @@
 remove_action( 'woocommerce_before_main_content', 'woocommerce_breadcrumb',                   20 );
 remove_action( 'woocommerce_before_main_content', 'woocommerce_output_content_wrapper',       10 );
 remove_action( 'woocommerce_after_main_content',  'woocommerce_output_content_wrapper_end',   10 );
+
 remove_action( 'woocommerce_sidebar',             'woocommerce_get_sidebar',                  10 );
+
 remove_action( 'woocommerce_after_shop_loop',     'woocommerce_pagination',                   10 );
 remove_action( 'woocommerce_before_shop_loop',    'woocommerce_result_count',                 20 );
 remove_action( 'woocommerce_before_shop_loop',    'woocommerce_catalog_ordering',             30 );
+
+
 add_action( 'woocommerce_before_main_content',    'storefront_before_content',                10 );
+
 add_action( 'woocommerce_after_main_content',     'storefront_after_content',                 10 );
 
 add_action( 'storefront_content_top',             'storefront_shop_messages',                 15 );
