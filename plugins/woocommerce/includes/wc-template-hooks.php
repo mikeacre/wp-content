7dd76c2cfa255c3c4942b5a1d991b4a33d2b0c96
mikeacre <mikeacre@gmail.com>
2018-01-29 08:23:03 -0500

    2
    2

--- plugins/woocommerce/includes/wc-template-hooks.php
+++ plugins/woocommerce/includes/wc-template-hooks.php
@@ -91,11 +91,11 @@ add_action( 'woocommerce_no_products_found', 'wc_no_products_found' );
  * @see woocommerce_template_loop_rating()
  */
 add_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_link_open', 10 );
-add_action( 'woocommerce_before_shop_loop_item_title', 'woocommerce_template_loop_product_thumbnail', 10 );
+add_action( 'woocommerce_before_shop_loop_item', 'woocommerce_template_loop_product_thumbnail', 20 );
 
 add_action( 'woocommerce_shop_loop_item_title', 'woocommerce_template_loop_product_title', 10 );
 
-add_action( 'oocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
+add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_price', 10 );
 add_action( 'woocommerce_after_shop_loop_item_title', 'woocommerce_template_loop_add_to_cart', 20 );
 
 add_action( 'woocommerce_after_shop_loop_item', 'woocommerce_template_loop_price', 10 );
