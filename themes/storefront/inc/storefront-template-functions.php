d5473150f4929711220594fe5c3417ec6550cc01
mikeacre <mikeacre@gmail.com>
2018-01-29 14:50:00 -0500

    d
    d

--- themes/storefront/inc/storefront-template-functions.php
+++ themes/storefront/inc/storefront-template-functions.php
@@ -302,6 +302,7 @@ if ( ! function_exists( 'storefront_homepage_header' ) ) {
 		?>
 		<header class="entry-header">
 			<?php
+
 			the_title( '<h1 class="entry-title">', '</h1>' );
 			?>
 		</header><!-- .entry-header -->
